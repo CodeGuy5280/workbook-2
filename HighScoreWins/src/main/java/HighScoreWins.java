2223642a7757e345d8a6e8afbbaaa581d8f2dd37
Alexander Leal <computerlanguage5280@gmail.com>
2025-04-16 11:11:01 -0500

    Adding code changes

---
+++ HighScoreWins/src/main/java/HighScoreWins.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class HighScoreWins {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+        System.out.println("Enter the score of the Home Team:" );
+        String homeTeam = in.nextLine().trim();
+        System.out.println("Enter the score of the Away Team:" );
+        String awayTeam = in.nextLine().trim();
+
+
+        //split input into teams and scores
+
+
+
+
+
+
+    }
+}
